2c01387488963abd609880a65a6fc00bf3cae19c
Шельмин Артём <shelmin85@mail.ru>
2025-05-13 19:31:09 +0700

    Список книг

---
+++ lab6/books.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: 'Александр Пушкин',
+        name: 'Евгений Онегин',
+        year: 1833,
+        price: '(450р)',
+        bestseller: true,
+        dostup: true,
+    },
+    {
+        author: 'Фёдор Достоевский',
+        name: 'Преступление и наказание',
+        year: 1866,
+        price: '(500р)',
+        bestseller: true,
+        dostup: true,
+    },
+    {
+        author: 'Лев Толстой',
+        name: 'Война и мир',
+        year: 1869,
+        price: '(750р)',
+        bestseller: false,
+        dostup: true,
+    },
+    {  
+        author: "Николай Гоголь", 
+        name: "Мёртвые души",
+        year: 1842, 
+        price: "(480р)", 
+        bestseller: false, 
+        dostup: true 
+    },
+    { 
+        author: "Михаил Лермонтов", 
+        name: "Герой нашего времени", 
+        year: 1840, 
+        price: "(420р)", 
+        bestseller: true, 
+        dostup: true 
+    },
+    { 
+        author: "Антон Чехов", 
+        name: "Вишнёвый сад", 
+        year: 1904, 
+        price: "(380р)", 
+        bestseller: false, 
+        dostup: false 
+    },
+    { 
+        author: "Иван Тургенев", 
+        name: "Отцы и дети", 
+        year: 1862, 
+        price: "(530р)", 
+        bestseller: true,
+        dostup: true 
+    },
+    { 
+        author: "Александр Солженицын", 
+        name: "Один день Ивана Денисовича", 
+        year: 1962, 
+        price: "(390р)", 
+        bestseller: false, 
+        dostup: true 
+    },
+    { 
+        author: "Михаил Булгаков", 
+        name: "Собачье сердце", 
+        year: 1925, 
+        price: "(440р)", 
+        bestseller: true, 
+        dostup: false 
+    },
+    { 
+        author: "Иван Гончаров", 
+        name: "Обломов", 
+        year: 1859, 
+        price: "(490р)", 
+        bestseller: false, 
+        dostup: true 
+    }
+];
